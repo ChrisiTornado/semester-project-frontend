a9ad8fc3790d45665b9daa2b99dc55aaf60e3bd7
Christophe Andunda <52066089+ChrisiTornado@users.noreply.github.com>
2024-06-21 13:15:18 +0200

    Create router.js

---
+++ src/router.js
@@ -0,0 +1,30 @@
+import { createRouter, createWebHistory } from 'vue-router';
+import Login from './components/Login.vue';
+import Register from './components/Register.vue';
+import TodoList from "@/components/TodoList.vue";
+
+
+const routes = [
+    {
+      path: '/',
+      name: 'Login',
+      component: Login
+    },
+    {
+      path: '/register',
+      name: 'Register',
+      component: Register
+    },
+    {
+      path: '/todos',
+      name: 'TodoList',
+      component: TodoList
+    }
+  ];
+
+  const router = createRouter({
+    history: createWebHistory(),
+    routes
+  });
+  
+  export default router;
